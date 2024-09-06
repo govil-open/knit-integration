2232a0e2a9c4d7a1a4c72eb5dc16afe8a47a476b
Govil Kumar <govil.kumar@bankopen.co>
2024-09-06 15:49:06 +0530

    Configured all HRIS Knit APIs as Enum

--- api/knit/knit_api.go
+++ api/knit/knit_api.go
@@ -21,15 +21,70 @@ const (
 	LEAVE_REQUEST
 	LEAVE_BALANCE
 	CREATE_LEAVE_REQUEST
-	ATTENANCE
+	ATTENDANCE
+	GET_COMPENSATION_PLANS
+	UPDATE_EMPLOYEE_COMPENSATION
+	EMPLOYEE_DOCUMENT
+	UPDATE_EMPLOYEE_DOCUMENT
+	DOCUMENT_CATEGORIES
+	UPDATE_EMPLOYEE
+	CREATE_EMPLOYEE
+	GET_POSITION_DETAILS
+	LIST_OF_VALUES
+	TERMINATE_EMPLOYEE
+	TERMINATION_REASON
+	GET_COMPANY_LIST
+	LIST_COMPANY_WIDE_DEDUCTION
+	CREATE_COMPANY_WIDE_DEDUCTION
+	UPDATE_COMPANY_WIDE_DEDUCTION
+	ENROLL_EMPLOYEE_IN_DEDUDCTION
+	UNENROLL_EMPLOYEE_IN_DEDUDCTION
+	UPDATE_EMPLOYEE_DEDUCTION
 )
 
 var apis = [...]API{
+	//Payroll API
 	{URL: "https://api.getknit.dev/v1.0/hr.employees.payroll.get", Type: GET},
+
+	//Leave APIs
 	{URL: "https://api.getknit.dev/v1.0/hr.employees.leave.requests", Type: GET},
 	{URL: "https://api.getknit.dev/v1.0/hr.employees.leave.balance", Type: GET},
 	{URL: "https://api.getknit.dev/v1.0/hr.employees.leave.create", Type: POST},
+
+	//Attendance API
 	{URL: "https://api.getknit.dev/v1.0/hr.employees.attendance", Type: GET},
+
+	//Compensation APIs
+	{URL: "https://api.getknit.dev/v1.0/hr.compensation.plans", Type: GET},
+	{URL: "https://api.getknit.dev/v1.0/hr.employee.compensation.update", Type: POST},
+
+	//Document APIs
+	{URL: "https://api.getknit.dev/v1.0/hr.employees.documents", Type: GET},
+	{URL: "https://api.getknit.dev/v1.0/hr.employees.document.upload", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.documents.categories", Type: GET},
+
+	//Employee APIs
+	{URL: "https://api.getknit.dev/v1.0/hr.employee.update", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.employee.create", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.documents.categories", Type: GET},
+
+	//List of values API
+	{URL: "https://api.getknit.dev/v1.0/hr.employees.field.values", Type: GET},
+
+	//Termination APIs
+	{URL: "https://api.getknit.dev/v1.0/hr.employee.terminate", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.termination.reasons", Type: GET},
+
+	//Company APIs
+	{URL: "https://api.getknit.dev/v1.0/hr.companies.list", Type: GET},
+
+	//Deduction APIs
+	{URL: "https://api.getknit.dev/v1.0/hr.deductions.list", Type: GET},
+	{URL: "https://api.getknit.dev/v1.0/hr.deductions.create", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.deductions.update", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.employee.deductions.enroll", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.employee.deductions.unenroll", Type: POST},
+	{URL: "https://api.getknit.dev/v1.0/hr.employee.deductions.update", Type: POST},
 }
 
 func (knitAPI KnitAPI) GetAPI() string {
